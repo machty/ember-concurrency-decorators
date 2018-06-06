98cc9ccf1e32455cf800223dfbbf4121ad4bc29a
Jan Buschtöns <jan.buschtoens@clark.de>
2018-06-06 20:57:18 +0200

    chore(deps): upgrade @ember-decorators/*

--- addon/index.js
+++ addon/index.js
@@ -1,17 +1,20 @@
 import { task as ecTask } from 'ember-concurrency';
-import { decorator } from '@ember-decorators/utils/decorator-wrappers';
-import extractValue from '@ember-decorators/utils/extract-value';
+import { computedDecorator } from "@ember-decorators/utils/computed";
+
+function extractValue(desc) {
+  return desc.value ||typeof desc.initializer === 'function' && desc.initializer() || null;
+}
 
 function taskify(desc) {
   if (!desc.writable) {
     throw new Error('ember-concurrency-decorators does not support using getters and setters');
   }
-  let value = extractValue(desc);
+  const value = extractValue(desc);
   return (typeof value === 'function') ? ecTask(value) : value;
 }
 
-export const task = decorator((target, key, desc) => taskify(desc));
-export const restartableTask = decorator((target, key, desc) => taskify(desc).restartable());
-export const dropTask = decorator((target, key, desc) => taskify(desc).drop());
-export const keepLatestTask = decorator((target, key, desc) => taskify(desc).keepLatest());
-export const enqueueTask = decorator((target, key, desc) => taskify(desc).enqueue());
+export const task = computedDecorator((target, key, desc) => taskify(desc));
+export const restartableTask = computedDecorator((target, key, desc) => taskify(desc).restartable());
+export const dropTask = computedDecorator((target, key, desc) => taskify(desc).drop());
+export const keepLatestTask = computedDecorator((target, key, desc) => taskify(desc).keepLatest());
+export const enqueueTask = computedDecorator((target, key, desc) => taskify(desc).enqueue());
