cef04ebbe3b18ceec634aab5684fc02650501f9c
Alex LaFroscia <alex@lafroscia.com>
2018-10-10 23:34:49 -0700

    chore: use standard QUnit syntax for modules

--- tests/unit/decorators-ts-test.ts
+++ tests/unit/decorators-ts-test.ts
@@ -1,3 +1,4 @@
+import { module, test } from 'qunit';
 import EmberObject from '@ember/object';
 import { run } from '@ember/runloop';
 import {
@@ -7,57 +8,56 @@ import {
   keepLatestTask,
   enqueueTask
 } from 'ember-concurrency-decorators';
-import { module, test } from 'qunit';
 
-module('Unit: decorators (TS)');
+module('Unit | decorators (TS)', function() {
+  test('a plethora of decorators', function(assert) {
+    assert.expect(5);
 
-test('a plethora of decorators', function(assert) {
-  assert.expect(5);
+    class Obj extends EmberObject {
+      @task
+      *doStuff() {
+        yield;
+        return 123;
+      }
 
-  class Obj extends EmberObject {
-    @task
-    *doStuff() {
-      yield;
-      return 123;
+      @restartableTask
+      *a() {
+        yield;
+        return 456;
+      }
+
+      @keepLatestTask
+      *b() {
+        yield;
+        return 789;
+      }
+
+      @dropTask
+      *c() {
+        yield;
+        return 12;
+      }
+
+      @enqueueTask
+      *d() {
+        yield;
+        return 34;
+      }
     }
 
-    @restartableTask
-    *a() {
-      yield;
-      return 456;
-    }
-
-    @keepLatestTask
-    *b() {
-      yield;
-      return 789;
-    }
-
-    @dropTask
-    *c() {
-      yield;
-      return 12;
-    }
-
-    @enqueueTask
-    *d() {
-      yield;
-      return 34;
-    }
-  }
-
-  let obj;
-  run(() => {
-    obj = Obj.create();
-    obj.get('doStuff').perform();
-    obj.get('a').perform();
-    obj.get('b').perform();
-    obj.get('c').perform();
-    obj.get('d').perform();
+    let obj;
+    run(() => {
+      obj = Obj.create();
+      obj.get('doStuff').perform();
+      obj.get('a').perform();
+      obj.get('b').perform();
+      obj.get('c').perform();
+      obj.get('d').perform();
+    });
+    assert.equal(obj.get('doStuff.last.value'), 123);
+    assert.equal(obj.get('a.last.value'), 456);
+    assert.equal(obj.get('b.last.value'), 789);
+    assert.equal(obj.get('c.last.value'), 12);
+    assert.equal(obj.get('d.last.value'), 34);
   });
-  assert.equal(obj.get('doStuff.last.value'), 123);
-  assert.equal(obj.get('a.last.value'), 456);
-  assert.equal(obj.get('b.last.value'), 789);
-  assert.equal(obj.get('c.last.value'), 12);
-  assert.equal(obj.get('d.last.value'), 34);
 });
