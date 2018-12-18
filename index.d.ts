33ea20fcb05988d914558a9b92a7b63ccbe35240
Jan Buschtöns <jan@buschtoens.me>
2018-12-18 17:23:56 +0100

    feat(ts): add some lo-fi types

---
+++ index.d.ts
@@ -0,0 +1,119 @@
+interface TaskGroupOptions {
+  restartable?: true;
+  drop?: true;
+  keepLatest?: true;
+  enqueue?: true;
+  maxConcurrency?: number;
+  cancelOn?: string;
+  group?: string;
+}
+
+interface TaskOptions {
+  restartable?: true;
+  drop?: true;
+  keepLatest?: true;
+  enqueue?: true;
+  evented?: true;
+  debug?: true;
+  maxConcurrency?: number;
+  on?: string;
+  cancelOn?: string;
+  group?: string;
+}
+
+type ExtractPropertyNamesOfType<T, S> = {
+  [K in keyof T]: T[K] extends S ? K : never
+}[keyof T];
+
+type GeneratorFn = () => IterableIterator<any>;
+
+type TaskMethodKeys<T> = ExtractPropertyNamesOfType<T, GeneratorFn>;
+type EncapsulatedTaskKeys<T> = ExtractPropertyNamesOfType<
+  T,
+  { perform: GeneratorFn }
+>;
+
+type TaskDecorator<O extends object> =
+  | ((target: O, propertyKey: EncapsulatedTaskKeys<O>) => void)
+  | ((
+      target: O,
+      propertyKey: TaskMethodKeys<O>,
+      descriptor: PropertyDescriptor
+    ) => void);
+
+type TaskGroupDecorator = PropertyDecorator;
+type TaskGroupDecoratorWithOptionalParams = TaskGroupDecorator &
+  ((options: TaskGroupOptions) => TaskGroupDecorator);
+
+export function task<O extends object>(
+  target: O,
+  propertyKey: TaskMethodKeys<O>,
+  descriptor: PropertyDescriptor
+): void;
+// export function task<O extends object>(
+//   target: O,
+//   propertyKey: EncapsulatedTaskKeys<O>
+// ): void;
+export function task<O extends object>(options: TaskOptions): TaskDecorator<O>;
+
+export function restartableTask<O extends object>(
+  target: O,
+  propertyKey: TaskMethodKeys<O>,
+  descriptor: PropertyDescriptor
+): void;
+// export function restartableTask<O extends object>(
+//   target: O,
+//   propertyKey: EncapsulatedTaskKeys<O>
+// ): void;
+export function restartableTask<O extends object>(
+  options: TaskOptions
+): TaskDecorator<O>;
+
+export function dropTask<O extends object>(
+  target: O,
+  propertyKey: TaskMethodKeys<O>,
+  descriptor: PropertyDescriptor
+): void;
+// export function dropTask<O extends object>(
+//   target: O,
+//   propertyKey: EncapsulatedTaskKeys<O>
+// ): void;
+export function dropTask<O extends object>(
+  options: TaskOptions
+): TaskDecorator<O>;
+
+export function keepLatestTask<O extends object>(
+  target: O,
+  propertyKey: TaskMethodKeys<O>,
+  descriptor: PropertyDescriptor
+): void;
+// export function keepLatestTask<O extends object>(
+//   target: O,
+//   propertyKey: EncapsulatedTaskKeys<O>
+// ): void;
+export function keepLatestTask<O extends object>(
+  options: TaskOptions
+): TaskDecorator<O>;
+
+export function enqueueTask<O extends object>(
+  target: O,
+  propertyKey: TaskMethodKeys<O>,
+  descriptor: PropertyDescriptor
+): void;
+// export function enqueueTask<O extends object>(
+//   target: O,
+//   propertyKey: EncapsulatedTaskKeys<O>
+// ): void;
+export function enqueueTask<O extends object>(
+  options: TaskOptions
+): TaskDecorator<O>;
+
+export const taskGroup: TaskGroupDecoratorWithOptionalParams;
+export const restartableTaskGroup: TaskGroupDecoratorWithOptionalParams;
+export const dropTaskGroup: TaskGroupDecoratorWithOptionalParams;
+export const keepLatestTaskGroup: TaskGroupDecoratorWithOptionalParams;
+export const enqueueTaskGroup: TaskGroupDecoratorWithOptionalParams;
+
+export function lastValue<O extends object, T extends keyof O>(
+  taskName: T
+): (target: O, propertyKey: T) => void;
