19f49ec41b72fa0c4476a19046a4aca1feefaf1d
James C. Davis <jamescdavis@gmail.com>
2020-06-18 03:58:44 -0400

    test: add tests for TS support wrapper

--- tests/unit/decorators-test.ts
+++ tests/unit/decorators-test.ts
@@ -76,6 +76,57 @@ module('Unit | decorators', function() {
     assert.equal(subject.get('d.last.value'), 34);
   });
 
+  test('Basic decorators functionality (using wrapper for TS support)', function(assert) {
+    assert.expect(5);
+
+    class TestSubject extends EmberObject {
+      @task
+      doStuff = task(function*() {
+        yield;
+        return 123;
+      });
+
+      @restartableTask
+      a = task(function*() {
+        yield;
+        return 456;
+      });
+
+      @keepLatestTask
+      b = task(function*() {
+        yield;
+        return 789;
+      });
+
+      @dropTask
+      c = task(function*() {
+        yield;
+        return 12;
+      });
+
+      @enqueueTask
+      d = task(function*() {
+        yield;
+        return 34;
+      });
+    }
+
+    let subject!: TestSubject;
+    run(() => {
+      subject = TestSubject.create();
+      subject.get('doStuff').perform();
+      subject.get('a').perform();
+      subject.get('b').perform();
+      subject.get('c').perform();
+      subject.get('d').perform();
+    });
+    assert.equal(subject.get('doStuff').last?.value, 123);
+    assert.equal(subject.get('a').last?.value, 456);
+    assert.equal(subject.get('b').last?.value, 789);
+    assert.equal(subject.get('c').last?.value, 12);
+    assert.equal(subject.get('d').last?.value, 34);
+  });
+
   // This has actually never worked.
   test('Encapsulated tasks', function(assert) {
     assert.expect(1);
@@ -98,5 +149,29 @@ module('Unit | decorators', function() {
     });
     // @ts-ignore
     assert.equal(subject.get('encapsulated.last.value'), 56);
+  });
+
+  test('Encapsulated tasks (using wrapper for TS support)', function(assert) {
+    assert.expect(1);
+
+    const ENCAPSULATED_TASK = {
+      privateState: 56,
+      *perform() {
+        yield;
+        return this.privateState;
+      }
+    };
+
+    class TestSubject extends EmberObject {
+      @task
+      encapsulated = task(ENCAPSULATED_TASK);
+    }
+
+    let subject!: TestSubject;
+    run(() => {
+      subject = TestSubject.create();
+      subject.get('encapsulated').perform();
+    });
+    assert.equal(subject.get('encapsulated').last?.value, 56);
   });
 });
--- tests/unit/last-value-test.ts
+++ tests/unit/last-value-test.ts
@@ -41,6 +41,34 @@ module('Unit | last-value', function(hooks) {
     );
   });
 
+  test('without a default value (using wrapper for TS support)', function(assert) {
+    class ObjectWithTask extends EmberObject {
+      @task
+      task = task(function*() {
+        return yield 'foo';
+      });
+
+      @lastValue('task')
+      value?: 'foo';
+    }
+
+    const instance = ObjectWithTask.create();
+    assert.strictEqual(
+      instance.get('value'),
+      undefined,
+      'it returns nothing if the task has not been performed'
+    );
+
+    instance.get('task').perform();
+    nextLoop();
+
+    assert.strictEqual(
+      instance.get('value'),
+      'foo',
+      'returning the last successful value'
+    );
+  });
+
   test('with a default value', function(assert) {
     class ObjectWithTaskDefaultValue extends EmberObject {
       @task
@@ -61,6 +89,35 @@ module('Unit | last-value', function(hooks) {
     );
 
     // @ts-ignore
+    instance.get('task').perform();
+    nextLoop();
+
+    assert.equal(
+      instance.get('value'),
+      'foo',
+      'returning the last successful value'
+    );
+  });
+
+  test('with a default value (using wrapper for TS support)', function(assert) {
+    class ObjectWithTaskDefaultValue extends EmberObject {
+      @task
+      task = task(function*() {
+        return yield 'foo';
+      });
+
+      @lastValue('task')
+      value = 'default value';
+    }
+
+    const instance = ObjectWithTaskDefaultValue.create();
+
+    assert.strictEqual(
+      instance.get('value'),
+      'default value',
+      'it returns the default value if the task has not been performed'
+    );
+
     instance.get('task').perform();
     nextLoop();
 
